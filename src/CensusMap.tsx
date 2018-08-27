58935779237a2789afbef8dcdbda581027d23f3e
Michael Shelton <michael@indify.io>
2018-08-26 20:06:46 -0400

    Adds leafly support

--- src/CensusMap.tsx
+++ src/CensusMap.tsx
@@ -1,57 +1,81 @@
 import * as React from 'react';
-import './App.css';
+import './CensusMap.css';
 import * as d3 from 'd3';
 import {FeatureCollection} from "geojson";
+import * as L from 'leaflet';
+import {GeoStream} from "d3";
 
-export interface ICensusMapProps {
-    width: number;
-    height: number;
-}
+export class CensusMap extends React.Component<{}, {}> {
 
-export class CensusMap extends React.Component<ICensusMapProps, {}> {
+    private leafletMapId = "leaflet-map-div";
+    private newYorkLatLong: [number, number] = [40.70, -73.94];
 
-    private svgNode: SVGSVGElement | null;
-
-    constructor(props: ICensusMapProps) {
+    constructor(props: {}) {
         super(props);
-        this.renderMapWithD3 = this.renderMapWithD3.bind(this)
+        this.renderMap = this.renderMap.bind(this)
     }
 
     public componentDidMount() {
-        this.renderMapWithD3();
+        this.renderMap();
     }
 
     public componentDidUpdate() {
-        this.renderMapWithD3();
+        this.renderMap();
     }
 
     public render() {
-        return <svg ref={node => this.svgNode = node} width={this.props.width} height={this.props.height}/>
+        return (
+            <div className="census-map-container">
+                <div id={this.leafletMapId}/>
+            </div>
+        );
     }
 
-    private renderMapWithD3() {
-        const svgNode = this.svgNode;
-        const width = this.props.width;
-        const height = this.props.height;
+    private renderMap() {
+        const map = new L.Map(this.leafletMapId, {center: this.newYorkLatLong, zoom: 13})
+            .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
 
-        const projection = d3.geoMercator()
-  					.center([-73.94, 40.70])
-  					.scale(50000)
-  					.translate([(width) / 2, (height)/2]);
+        const svg = d3.select(map.getPanes().overlayPane).append("svg");
+        const g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
-        const path = d3.geoPath().projection(projection);
-        const svg = d3.select(svgNode);
 
-        d3.json("2010_Census_Tracts.geojson").then( (geoData: FeatureCollection) => {
-           svg.selectAll("path")
-               .data(geoData.features)
-               .enter()
-               .append("path")
-               .attr("d", path)
-               .style("fill", "steelblue")
-               .style("stroke", "black")
-               .style("stroke-width", "1");
+        d3.json("2010_Census_Tracts.geojson").then((geoData: FeatureCollection) => {
+            const transform = d3.geoTransform({point: projectPoint});
+            const path = d3.geoPath().projection(transform);
 
-        }).catch( (error) => console.log(error) );
+            const feature = g.selectAll("path")
+                .data(geoData.features)
+                .enter()
+                .append("path");
+
+            map.on({
+                "viewreset": reset,
+                "zoom": reset,
+            });
+            reset();
+
+            function reset() {
+                const bounds = path.bounds(geoData);
+                const topLeft = bounds[0];
+                const bottomRight = bounds[1];
+
+                svg.attr("width", bottomRight[0] - topLeft[0])
+                    .attr("height", bottomRight[1] - topLeft[1])
+                    .style("left", topLeft[0] + "px")
+                    .style("top", topLeft[1] + "px");
+
+                g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+
+                feature.attr("d", path);
+            }
+
+            // Use Leaflet to implement a D3 geometric transformation.
+            function projectPoint(this: { stream: GeoStream }, x: number, y: number) {
+                const point = map.latLngToLayerPoint(new L.LatLng(y, x));
+                this.stream.point(point.x, point.y);
+            }
+
+
+        }).catch((error) => console.log(error));
     }
 }
